fcab94fa5b25ebc8138ce45f2fd5b94c0fa128b9
FelipeG <felipe.guerrero.padilla@gmail.com>
2025-06-02 12:56:14 -0400

    "dia-11"

---
+++ dia-11/main.js
@@ -0,0 +1,113 @@
+// aqui van comentarios en una sola fila
+
+/* */
+
+// 1. Escribe un comentario en una línea
+
+//R : Un comentario en una linea :v
+
+
+// 2. Escribe un comentario en varias líneas
+
+/*R : Un comentario
+en varias
+lineas :v  */
+
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let nombre = "Felipe" ;
+
+let numerito = -5 ;
+
+let programador = true ;
+let noProgramador = false ;
+
+let noDefinida;
+
+let taNulo = null ;
+
+let simbolito = Symbol("numero0") ;
+
+let numeroGrandecito = 569843859701548424851n ;
+
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(nombre);
+console.log(numerito);
+console.log(programador);
+console.log(noProgramador);
+console.log(noDefinida);
+console.log(taNulo);
+console.log(simbolito);
+console.log(numeroGrandecito);
+
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof nombre);
+console.log(typeof numerito);
+console.log(typeof programador);
+console.log(typeof noDefinida);
+console.log(typeof taNulo);
+console.log(typeof simbolito);
+console.log(typeof numeroGrandecito);
+
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+let nombre1 = "Felipe" ;
+
+let numerito2 = -5 ;
+
+let programador4 = true ;
+let noProgramador5 = false ;
+
+let noDefinida6;
+
+let taNulo7 = null ;
+
+let simbolito8 = Symbol("numero0") ;
+
+let numeroGrandecito9 = 569843859701548424851n ;
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+ nombre1 = "100" ;
+
+ numerito2 = "felipe" ;
+
+ programador4 = "si" ;
+ noProgramador5 = "no" ;
+
+ noDefinida6 = "estoy definida :v" ;
+
+ taNulo7 = true ;
+
+simbolito8 = Symbol("numero0") ; //segun yo no se puede modificar//
+
+ numeroGrandecito9 ="" ;
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const segundoNombre = "Ignacio" ;
+
+const numeritoPositivo = 5 ;
+
+const diseñador = true ;
+const nodiseñador = false ;
+
+const nose =("");
+
+const estaNulo = null ;
+
+const simbolito2 = Symbol("numero10") ;
+
+const numerosGrandecitos = 569843859701548425104n ;
+
+// 9. A continuación, modifica los valores de las constantes
+
+//R : Segun yo eso no se puede
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
