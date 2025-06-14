168b59bbcc36b6691e980e53bd6fd76ea8cc6687
FelipeG <felipe.guerrero.padilla@gmail.com>
2025-06-13 22:52:35 -0400

    "dia-14-06-2025"

--- Portafolio/proyectos.js
+++ Portafolio/proyectos.js
@@ -2,32 +2,32 @@ let proyectos = [
   {
     src: "./fotos/proyecto1.png",
     alt: "proyecto1",
-    enlace: "./calificada 1/caliicada 1.html",
+    enlace: "../calificada 1/caliicada 1.html",
   },
   {
     src: "./fotos/proyecto2.png",
     alt: "proyecto2",
-    enlace: "./calificada 2/calificada 2.html",
+    enlace: "../calificada 2/calificada 2.html",
   },
   {
     src: "./fotos/proyecto3.png",
     alt: "proyecto3",
-    enlace: "./calificada 3/calificada3.html",
+    enlace: "../calificada 3/calificada3.html",
   },
   {
     src: "./fotos/calculadora.png",
     alt: "calculadora",
-    enlace: "./calificada 5/index.html",
+    enlace: "../calificada 5/index.html",
   },
   {
     src: "./fotos/bancario.png",
     alt: "bancario",
-    enlace: "./calificada-6/calificada-6.html",
+    enlace: "../calificada-6/calificada-6.html",
   },
   {
     src: "./fotos/tienda.png",
     alt: "tienda",
-    enlace: "./calificada-7/calificada7.html",
+    enlace: "../calificada-7/calificada7.html",
   },
 ];
 
